00a5caf773a7d17dda49920b0fdd63ac386d15ff
victorsmits <victor.smits@student.vinci.be>
2018-10-30 15:18:51 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/while_loop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/while_loop.dir/main.c.o"
+  "while_loop.pdb"
+  "while_loop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/while_loop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
